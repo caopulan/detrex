85994a36245f5b8eec9621787ce230c165ffec91
sariel <2788787973@qq.com>
2023-01-07 10:05:37 +0800

    add configs

---
+++ configs/common/data/coco_vitdet_640.py
@@ -0,0 +1,56 @@
+from omegaconf import OmegaConf
+
+import detectron2.data.transforms as T
+from detectron2.config import LazyCall as L
+from detectron2.data import (
+    DatasetMapper,
+    build_detection_test_loader,
+    build_detection_train_loader,
+    get_detection_dataset_dicts,
+)
+from detectron2.evaluation import COCOEvaluator
+
+from detrex.data import DetrDatasetMapper
+
+dataloader = OmegaConf.create()
+
+image_size = 640
+dataloader.train = L(build_detection_train_loader)(
+    dataset=L(get_detection_dataset_dicts)(names="coco_2017_train"),
+    mapper=L(DetrDatasetMapper)(
+        is_train=True,
+        augmentation=[
+            L(T.RandomFlip)(horizontal=True),  # flip first
+            L(T.ResizeScale)(
+                min_scale=0.1, max_scale=2.0, target_height=image_size, target_width=image_size
+            ),
+            L(T.FixedSizeCrop)(crop_size=(image_size, image_size), pad=False),
+        ],
+        img_format="RGB",
+        mask_on=False,
+        augmentation_with_crop=None,
+        # use_instance_mask=True,
+    ),
+    total_batch_size=16,
+    num_workers=4,
+)
+
+# dataloader.train.mapper.recompute_boxes = True
+
+dataloader.test = L(build_detection_test_loader)(
+    dataset=L(get_detection_dataset_dicts)(names="coco_2017_val", filter_empty=False),
+    mapper=L(DetrDatasetMapper)(
+        augmentation=[
+            L(T.ResizeShortestEdge)(short_edge_length=image_size, max_size=image_size),
+        ],
+        augmentation_with_crop=None,
+        is_train=False,
+        mask_on=False,
+        img_format="RGB",
+    ),
+    num_workers=4,
+)
+
+dataloader.evaluator = L(COCOEvaluator)(
+    dataset_name="${..test.dataset.names}",
+)
